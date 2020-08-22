436c71a81ac681d6cbb2230db02068ac90f71dbc
Valerii Makhniuk <vmakhnyuk@yandex.ru>
2020-08-22 13:24:57 +0300

    add middleware to mutate POST request data with unique id

--- src/server/run-mock-server.js
+++ src/server/run-mock-server.js
@@ -1,11 +1,19 @@
 const path = require('path');
 
 const jsonServer = require('json-server');
+const faker = require('faker');
 const router = jsonServer.router(path.join(__dirname, 'db.json'));
 const server = jsonServer.create();
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
+server.use(jsonServer.bodyParser);
+server.use((req, res, next) => {
+  if (req.method === 'POST' || req.method === 'PUT') {
+    req.body.id = faker.random.uuid();
+  }
+  next();
+});
 server.use(router);
 
 /**
