698e3063c52525cdd9a5c49cbc5fd273b5e1cf0b
Valerii Makhniuk <vmakhnyuk@yandex.ru>
2020-08-25 12:49:23 +0300

    apply chartReducer to rootReducer

--- src/store/rootReducer.ts
+++ src/store/rootReducer.ts
@@ -1,9 +1,11 @@
 import { combineReducers } from '@reduxjs/toolkit';
 
 import { accountsReducer } from './accounts/accountsSlice';
+import { chartReducer } from './chart/chartSlice';
 
 export const rootReducer = combineReducers({
   accounts: accountsReducer,
+  chart: chartReducer,
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
